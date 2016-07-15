202c898958f7945f139abbc2545cc07d5378456d
Ruth <rebagley@haverford.edu>
2016-07-15 10:12:34 -0400

    adding some files to react

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var app = express();
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var FacebookStrategy = require('passport-facebook');
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
