e62065004c555d28cde0adece51413e7d6030a03
Ruth <rebagley@haverford.edu>
2016-07-18 13:39:02 -0400

    adding route folder

---
+++ route/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/tutorial',function(req,res,next){
+  res.render('tutorial')
+})
+
+router.get('/settings',function(req,res,next){
+  res.render('settings')
+})
+
+router.post('/settings',function(req,res,next){
+
+})
+
+router.get('/game',function(req,res,next){
+  res.render('gameView')
+})
+
+module.exports = router;
