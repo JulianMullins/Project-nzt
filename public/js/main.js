cb769fdfac0caa372ffa0a7d0c85c954cc41de4c
Virginia Van Keuren <virginiavankeuren2017@u.northwestern.edu>
2016-07-18 20:08:12 -0400

    added cool transition to menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
   //SHOWING FULL MENU
-  $(".bt-menu-trigger-out").click(function(){
-      $("#root").css('margin', '30px 30px 30px 90px;');
-  });
+  // if($('#bt-menu').is('.bt-menu', '.bt-menu-open')){
+  //     $("#root").css("margin", "30px 30px 30px 90px");
+  //   };
+  //
+  // if($('#bt-menu').is('.bt-menu', '.bt-menu-close')){
+  //     $("#root").css("margin", "0");
+  //   };
+
+
+$(".bt-menu-trigger").on('click', function(){
+  if($('#bt-menu').is('.bt-menu-open')){
+      $("#root").css("margin", "30px 30px 30px 90px");
+      $("#root").css("transition-duration", ".4s");
+      // $('.map').css('margin', '');
+    };
+
+  if($('#bt-menu').is('.bt-menu-close')){
+      $('#root').css('margin', '');
+      $("#root").css("transition-duration", ".2s");
+    };
+ });
 });
