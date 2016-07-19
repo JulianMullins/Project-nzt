97c10b046ffebeca3de74b6b6f286ffb9774ce9f
Julian Mullins <mullinsjulian@gmail.com>
2016-07-18 20:10:42 -0400

    ignore

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
-$(document).ready(function () {
-  //SHOWING FULL MENU
-  $(".bt-menu-trigger-out").click(function(){
-      $("#root").css('margin', '30px 30px 30px 90px;');
-  });
-});
+// $(document).ready(function () {
+//   //SHOWING FULL MENU
+//   $(".bt-menu-trigger").on('click', function(){
+//   		console.log("Clicked");
+//   	if( $('#root').css('margin', '0')) {
+//   		console.log('margin 0');
+//   	}
+//       $("#root").css('margin', '30px 30px 30px 90px');
+//   });
+
+// });
