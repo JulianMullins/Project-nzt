0d30a8cfc1ebf0eef885a84ca5cbcf5520690498
Julian Mullins <mullinsjulian@gmail.com>
2016-07-18 20:12:00 -0400

    menu transition

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,5 @@
-<<<<<<< HEAD
-// $(document).ready(function () {
-//   //SHOWING FULL MENU
-//   $(".bt-menu-trigger").on('click', function(){
-//   		console.log("Clicked");
-//   	if( $('#root').css('margin', '0')) {
-//   		console.log('margin 0');
-//   	}
-//       $("#root").css('margin', '30px 30px 30px 90px');
-//   });
-
-// });
-=======
 $(document).ready(function () {
-  //SHOWING FULL MENU
-  // if($('#bt-menu').is('.bt-menu', '.bt-menu-open')){
-  //     $("#root").css("margin", "30px 30px 30px 90px");
-  //   };
-  //
-  // if($('#bt-menu').is('.bt-menu', '.bt-menu-close')){
-  //     $("#root").css("margin", "0");
-  //   };
-
-
+	
 $(".bt-menu-trigger").on('click', function(){
   if($('#bt-menu').is('.bt-menu-open')){
       $("#root").css("margin", "30px 30px 30px 90px");
@@ -35,4 +13,3 @@ $(".bt-menu-trigger").on('click', function(){
     };
  });
 });
->>>>>>> origin/master
