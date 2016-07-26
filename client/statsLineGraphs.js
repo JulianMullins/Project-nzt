fe632834f4c22c40ec2cf98e7f0639cef88a90d5
tconcannon <taycon@seas.upenn.edu>
2016-07-26 16:58:13 -0400

    charttssszzzz

--- client/statsLineGraphs.js
+++ client/statsLineGraphs.js
@@ -5,79 +5,79 @@ var Chart = require('react-d3-core').Chart;
 // require `react-d3-basic` for Line chart component.
 var LineChart = require('react-d3-basic').LineChart;
 
-//get funciton, res.json()
-// //use req.user
-// var chartData = {labels: ["January", "February", "March", "April", "May", "June", "July"],
-//     datasets: [
-//         {
-//             score: 123      
-//         },
-//          {
-//             score: 145
-//         },
-//         {
-//             score: 125
-//         },
-//         {
-//             score: 245
-//         },
-//         {
-//             score: 341
-//         }
-//     ]};
 
-// var width = 700,
-//     height = 300,
-//     margins = {left: 100, right: 100, top: 50, bottom: 50},
-//     title = "User sample",
-//     // chart series,
-//     // field: is what field your data want to be selected
-//     // name: the name of the field that display in legend
-//     // color: what color is the line
-//     chartSeries = [
-//       {
-//         field: 'BMI',
-//         name: 'BMI',
-//         color: '#ff7f0e'
-//       }
-//     ],
-//     // your x accessor
-//     x = function(d) {
-//       return d.index;
-//     }
+var chartData = [
+        {
+            score: 123      
+        },
+         {
+            score: 145
+        },
+        {
+            score: 125
+        },
+        {
+            score: 245
+        },
+        {
+            score: 341
+        }
+    ];
+
+var width = 700,
+    height = 300,
+    margins = {left: 100, right: 100, top: 50, bottom: 50},
+    title = "User sample",
+    // chart series,
+    // field: is what field your data want to be selected
+    // name: the name of the field that display in legend
+    // color: what color is the line
+    chartSeries = [
+      {
+        field: 'score',
+        name: 'score',
+        color: '#ff7f0e'
+      }
+    ],
+    // your x accessor
+    x = function(d) {
+      return d.index;
+    }
+
+    //console.log(x)
 
 var MyComponent = React.createClass({
     componentDidMount: function(){
 //fetch call
 
-fetch('/taco', {method: 'GET'})
-.then(function(response){
-    console.log(response, '38')
-    return response.json()
-    })
-.then(function(responseJson){
-    console.log(responseJson,'41')
-})
+// fetch('/taco', {method: 'GET'})
+// .then(function(response){
+//     console.log(response, '38')
+//     return response.json()
+//     })
+// .then(function(responseJson){
+//     console.log(responseJson,'41')
+// })
     },
   render: function() {
-    return <div>TEST</div>
-    // return <Chart
-    //   title={title}
-    //   width={width}
-    //   height={height}
-    //   margins= {margins}
-    //   >
-    //   <LineChart
-    //     margins= {margins}
-    //     title={title}
-    //     data={chartData}
-    //     width={width}
-    //     height={height}
-    //    // chartSeries={chartSeries}
-    //     x={x}
-    //   />
-   // </Chart>
+    //return <div>TEST</div>
+    return <Chart
+        title={title}
+        width={width}
+        height={height}
+        margins= {margins}
+        >
+      <LineChart
+        margins= {margins}
+        title={title}
+        data={chartData}
+        width={width}
+        height={height}
+        chartSeries={chartSeries}
+        x={x}
+      />
+   </Chart>
   }
 });
-
+      
 module.exports = MyComponent
