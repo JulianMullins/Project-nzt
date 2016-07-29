7d4647c784358faa93f56f19e01d5bec5fc7d017
tconcannon <taycon@seas.upenn.edu>
2016-07-29 14:51:43 -0400

    sorting stats in statsLineGraphs

--- client/statsLineGraphs.js
+++ client/statsLineGraphs.js
@@ -6,48 +6,50 @@ var AreaChart = rd3.AreaChart;
 var axios = require('axios')
 var _=require('underscore')
 var stats;
-var mode;
+var dates=[];
+// var lineData1 = [{name: 'scores', values:[]}];
+// var lineData2 = [{name: 'reaction times', values:[]}];
 
 var MyComponent = React.createClass({
-    componentDidMount: function(){
-//fetch call
-
+  getInitialState: function(){
+    return{
+      lineData1: [{name: 'scores', values:[{x:0,y:0}]}],
+      lineData2: [{name: 'reaction times', values:[{x:0,y:0}]}]
+    }
+  },
+componentDidMount: function(){
+ // console.log(this.state, 'state')
 axios.get('/taco', {withCredentials: true})
 .then(function(responseJson){
-    //console.log(responseJson.data,'41')
+  //console.log(this.state,'state')
+    // //console.log(responseJson.data,'41')
+    this.state.lineData1[0].values=[];
+    this.state.lineData2[0].values=[];
     stats=responseJson.data.stats;
-    console.log(stats,'stats')
-})
-    },
+    // //console.log(stats,'stats')
+    _.map(stats, function(item, index){
+      this.state.lineData1[0].values.push({x: item.score, y:index})
+      this.state.lineData2[0].values.push({x: item.reactionTimes[0], y: index})
+      dates.push(item.dateAchieved)
+    }.bind(this))
+    console.log(this,'this')
+  }.bind(this))
+  // .then(function(){
+  //   this.setState({
+  //     lineData1: this.state.lineData1,
+  //     lineData2: this.state.lineData2
+  //   })
+  // })
+},
   render: function() {
-    var lineData1 = [
-      { 
-        name: 'series1',
-        values: [ { x: 0, y: 20 }, { x: 1, y: 30 }, { x: 2, y: 10 }, { x: 3, y: 5 }, { x: 4, y: 8 }, { x: 5, y: 15 }, { x: 6, y: 10 } ]
-      }
-    ];
-    var lineData2 = [
-      { 
-        name: 'series1',
-        values: [ { x: 0, y: 20 }, { x: 1, y: 30 }, { x: 2, y: 10 }, { x: 3, y: 5 }, { x: 4, y: 8 }, { x: 5, y: 15 }, { x: 6, y: 10 } ]
-      },
-      {
-        name: 'series2',
-        values : [ { x: 0, y: 8 }, { x: 1, y: 5 }, { x: 2, y: 20 }, { x: 3, y: 12 }, { x: 4, y: 4 }, { x: 5, y: 6 }, { x: 6, y: 2 } ]
-      },
-      {
-        name: 'series3',
-        values: [ { x: 0, y: 0 }, { x: 1, y: 5 }, { x: 2, y: 8 }, { x: 3, y: 2 }, { x: 4, y: 6 }, { x: 5, y: 4 }, { x: 6, y: 2 } ]
-      } 
-    ];
-    return (<div><LineChart
-        data={lineData1}
+   return (<div><LineChart
+        data={this.state.lineData1}
         width={500}
         height={300}
         title="Line Chart"
         />
         <AreaChart
-        data={lineData2}
+        data={this.state.lineData2}
         width={500}
         height={300}
         title="Area Chart"
