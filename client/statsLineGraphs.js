0a8fa9278b261f74481e9c2dc2ac31314466a5cc
tconcannon <taycon@seas.upenn.edu>
2016-08-01 12:25:22 -0400

    adding area chart for reaction times

--- client/statsLineGraphs.js
+++ client/statsLineGraphs.js
@@ -2,56 +2,51 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var rd3 = require('react-d3-basic');
 var LineChart = rd3.LineChart;
-var AreaChart = rd3.AreaChart;
+var AreaStackChart = rd3.AreaStackChart;
 var _=require('underscore')
 var axios=require('axios')
 var stats;
 var dates=[];
-// var lineData1 = [{name: 'scores', values:[]}];
-// var lineData2 = [{name: 'reaction times', values:[]}];
-//var Legend = require('react-d3-core').Legend;
     
  
 var margins = {left: 100, right: 100, top: 50, bottom: 50}
 var MyComponent = React.createClass({
   getInitialState: function(){
     return{
-      data: [{"score": 0, "index": 0}],
-      chartSeries: [{field: 'score', name: 'Score', color: '#ff7f0e',
-            style: {"stroke-width": 2, "stroke-opacity": .2, "fill-opacity": .2}}]
-          }
+      data: [{"score": 0, "index": 0, avgR:0, maxR:0, minR:0}],
+      chartSeries1: [{field: 'score', name: 'Score', color: '#ff7f0e',
+            style: {"strokeWidth": 2, "fillOpacity": .2}}]
+          },
+      chartSeries2: [{field: 'avgR', name: 'Average Reaction Time', color: '#ff7f0e',
+            style: {"strokeWidth": 2, "fillOpacity": .2}},
+            {field: 'maxR', name: 'Max Reaction Time', color: '#ff7f0e',
+            style: {"strokeWidth": 2, "fillOpacity": .2}},
+            {field: 'minR', name: 'Min Reaction Time', color: '#ff7f0e',
+            style: {"strokeWidth": 2, "fillOpacity": .2}}]
+          },
     },
 componentDidMount: function(){
  console.log(this.state, 'state')
 axios.get('/taco', {withCredentials: true})
 .then(function(responseJson){
-    // this.state.lineData1[0].values=[];
-    // this.state.lineData2[0].values=[];
-    // this.state.lineData2[1].values=[];
-    // this.state.lineData2[2].values=[];
     stats=responseJson.data.stats;
-    console.log(stats, 'stats')
-   // _.map(stats, function(item, index){
-      // if(item.score===0 || item.reactionTimes[0]===0){
-      //   return
-      // }
-      // this.state.lineData1[0].values.push({x: index, y:item.score})
-      // this.state.lineData2[0].values.push({x: index, y:item.reactionTimes[0]*1.2/1000})
-      // this.state.lineData2[1].values.push({x: index, y:item.reactionTimes[0]/1000})
-      // this.state.lineData2[2].values.push({x: index, y:(item.reactionTimes[0]*.8/1000)})
-      // var date=item.dateAchieved.split('Z');
-      // date=date[0].split('T');
-      // date=date[0].split('-')
-      // dates.push({full: item.dateAchieved, splitDate: date})
-   // }.bind(this))
+    //console.log(stats, 'stats')
+    this.state.data=[];
+   _.map(stats, function(item, index){
+      if(item.score===0 || !item.reactionTimes[0]){
+        return
+       }
+      this.state.data.push(item)
+      this.state.data[this.state.data.length-1].index=index;
+   }.bind(this))
+   //console.log(this.state.data,'new state')
    // console.log(dates,'dates')
    // console.log(this.state.lineData2[2].values)
   }.bind(this))
   .then(function(){
-    // this.setState({
-    //   lineData1: this.state.lineData1,
-    //   lineData2: this.state.lineData2
-    // })
+    this.setState({
+      data: this.state.data
+    })
 }.bind(this))
 },
   render: function() {
@@ -73,15 +68,6 @@ axios.get('/taco', {withCredentials: true})
 module.exports = MyComponent
 
 
-// <Legend
-//       width= {width}
-//       height= {height}
-//       margins= {margins}
-//       legendClassName= {legendClassName}
-//       legendPosition= {legendPosition}
-//       legendOffset= {legendOffset}
-//       chartSeries = {chartSeries}
-//     />
 
 
  //        <AreaChart
