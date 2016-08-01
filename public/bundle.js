678db4750de8c53d45255067e53f1618ce3e9439
tconcannon <taycon@seas.upenn.edu>
2016-08-01 12:07:55 -0400

    base case using react-d3-basic

--- public/bundle.js
+++ public/bundle.js
@@ -4166,84 +4166,62 @@ var dates = [];
 // var lineData2 = [{name: 'reaction times', values:[]}];
 //var Legend = require('react-d3-core').Legend;
 
-var data = [{
-  "age": 0,
-  "index": 0
-}, {
-  "age": .2,
-  "index": 1
-}, {
-  "age": .7,
-  "index": 2
-}, {
-  "age": .4,
-  "index": 3
-}];
 
-var chartSeries = [{
-  field: 'age',
-  name: 'Age',
-  color: '#ff7f0e',
-  style: {
-    "stroke-width": 2,
-    "stroke-opacity": .2,
-    "fill-opacity": .2
-  }
-}];
-var x = function x(d) {
-  return d.index;
-};
-
+var margins = { left: 100, right: 100, top: 50, bottom: 50 };
 var MyComponent = React.createClass({
   displayName: 'MyComponent',
 
   getInitialState: function getInitialState() {
     return {
-      lineData1: [{ name: 'scores', values: [{ x: 0, y: 0 }] }],
-      lineData2: [{ name: 'max reaction time', values: [{ x: 0, y: 0 }] }, { name: 'avg reaction time', values: [{ x: 0, y: 0 }] }, { name: 'min reaction time', values: [{ x: 0, y: 0 }] }]
+      data: [{ "score": 0, "index": 0 }],
+      chartSeries: [{ field: 'score', name: 'Score', color: '#ff7f0e',
+        style: { "stroke-width": 2, "stroke-opacity": .2, "fill-opacity": .2 } }]
     };
   },
   componentDidMount: function componentDidMount() {
-    // console.log(this.state, 'state')
-    // axios.get('/taco', {withCredentials: true})
-    // .then(function(responseJson){
-    //     this.state.lineData1[0].values=[];
-    //     this.state.lineData2[0].values=[];
-    //     this.state.lineData2[1].values=[];
-    //     this.state.lineData2[2].values=[];
-    //     stats=responseJson.data.stats;
-    //     _.map(stats, function(item, index){
-    //       if(item.score===0 || item.reactionTimes[0]===0){
-    //         return
-    //       }
-    //       this.state.lineData1[0].values.push({x: index, y:item.score})
-    //       this.state.lineData2[0].values.push({x: index, y:item.reactionTimes[0]*1.2/1000})
-    //       this.state.lineData2[1].values.push({x: index, y:item.reactionTimes[0]/1000})
-    //       this.state.lineData2[2].values.push({x: index, y:(item.reactionTimes[0]*.8/1000)})
-    //       var date=item.dateAchieved.split('Z');
-    //       date=date[0].split('T');
-    //       date=date[0].split('-')
-    //       dates.push({full: item.dateAchieved, splitDate: date})
-    //     }.bind(this))
-    //    // console.log(dates,'dates')
-    //    // console.log(this.state.lineData2[2].values)
-    //   }.bind(this))
-    //   .then(function(){
-    //     this.setState({
-    //       lineData1: this.state.lineData1,
-    //       lineData2: this.state.lineData2
-    //     })
-    // }.bind(this))
+    console.log(this.state, 'state');
+    axios.get('/taco', { withCredentials: true }).then(function (responseJson) {
+      // this.state.lineData1[0].values=[];
+      // this.state.lineData2[0].values=[];
+      // this.state.lineData2[1].values=[];
+      // this.state.lineData2[2].values=[];
+      stats = responseJson.data.stats;
+      console.log(stats, 'stats');
+      // _.map(stats, function(item, index){
+      // if(item.score===0 || item.reactionTimes[0]===0){
+      //   return
+      // }
+      // this.state.lineData1[0].values.push({x: index, y:item.score})
+      // this.state.lineData2[0].values.push({x: index, y:item.reactionTimes[0]*1.2/1000})
+      // this.state.lineData2[1].values.push({x: index, y:item.reactionTimes[0]/1000})
+      // this.state.lineData2[2].values.push({x: index, y:(item.reactionTimes[0]*.8/1000)})
+      // var date=item.dateAchieved.split('Z');
+      // date=date[0].split('T');
+      // date=date[0].split('-')
+      // dates.push({full: item.dateAchieved, splitDate: date})
+      // }.bind(this))
+      // console.log(dates,'dates')
+      // console.log(this.state.lineData2[2].values)
+    }.bind(this)).then(function () {
+      // this.setState({
+      //   lineData1: this.state.lineData1,
+      //   lineData2: this.state.lineData2
+      // })
+    }.bind(this));
   },
   render: function render() {
+    var x = function x(d) {
+      return d.index;
+    };
     return React.createElement(
       'div',
       null,
       ' ',
       React.createElement(LineChart, {
         className: 'topStatsGraph',
-        data: data,
-        chartSeries: chartSeries,
+        data: this.state.data,
+        margins: margins,
+        chartSeries: this.state.chartSeries,
         width: 1100,
         height: 400,
         x: x
