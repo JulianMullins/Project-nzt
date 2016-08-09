23d0755d4cb2074f6e659723c1abbaba288a896e
Julian Mullins <mullinsjulian@gmail.com>
2016-08-09 16:06:49 -0400

    stats page

--- client/statsLineGraphs.js
+++ client/statsLineGraphs.js
@@ -37,7 +37,7 @@ var MyComponent = React.createClass({
   getInitialState: function(){
     return{
       data: [{"score": 0, "index": 0, avgR:0, maxR:0, minR:0}],
-      chartSeries1: [{field: 'score', name: 'Score', color: '#ff7f0e',
+      chartSeries1: [{field: 'score', name: 'Score', color: '#F13542',
             style: {"strokeWidth": 2, "fillOpacity": .2}}],
       chartSeries2: [{field: 'maxR', name: 'Max Reaction Time'},
                       {field: 'avgR', name: 'Average Reaction Time'},
@@ -93,12 +93,45 @@ axios.get('/getStats', {withCredentials: true})
       data: this.state.data,
       alert: ' '
     })
-    }
-    
-}.bind(this))
+    }  
+  }.bind(this))
+
+//get highest n-level
+axios.get('/getMaxN')
+      .then(function(response){
+        var maxN = 0;
+        for(var key in response.data.maxN) {
+          if(response.data.maxN[key] > maxN) {
+            maxN = response.data.maxN[key];
+          }
+        }
+        this.setState({
+          maxN: maxN,
+        })
+      }.bind(this))
+
+//get highest score
+axios.get('/myHighScores').then(function(response) {
+      var highScore = 0;
+      for (var i=0; i<response.data.length; i++) {
+        if(response.data[i].score > highScore) {
+          highScore = response.data[i].score;
+        }
+      }
+      this.setState({highScore: highScore});
+    }.bind(this));
+
+//get user full name
+axios.get('/homeUserInfo')
+      .then(function(response){
+        console.log(response.data)
+        this.setState({
+          fullName:response.data.name,
+        })
+      }.bind(this))
 },
   render: function() {
-    console.log(stats,'stats')
+    // console.log(stats,'stats')
     var x = function(d) {
       return d.dateAchieved;
     }
@@ -112,43 +145,79 @@ axios.get('/getStats', {withCredentials: true})
       )
     }
     else{
-      return (<div>
-       <LineChart
-        className='StatsScoreGraph'
-        data={this.state.data}
-        margins={margins}
-        chartSeries={this.state.chartSeries1}
-        width={1100}
-        height={500}
-        title={'Score History'}
-        x={x}
-        xScale={xScale}
-        yAxisClassName= {'lineY'}
-        xAxisClassName= {'lineX'}
-        yLabel={'Scores'}
-        //x axis includes first and last day of play (for time range)
-        xLabel={'Gameplay from '+dayA+', '+ monthA + ' '+ dateA+ ', ' +yearA + ' to ' +dayB+', '+ monthB + ' '+ dateB+ ', ' +yearB}
-        />
-    <AreaChart
-      width={1100}
-      height={500}
-      title='TITLE'
-      data= {this.state.data}
-      className='StatsReactionGraph'
-      margins={margins}
-      chartSeries= {this.state.chartSeries2}
-      yAxisClassName= {'areaY'}
-      xAxisClassName= {'areaX'}
-      x= {x}
-      xScale={xScale}
-      yLabel={'Reaction Times (ms)'}
-      //x axis includes first and last day of play (for time range)
-      xLabel={'Gameplay from '+dayA+', '+ monthA + ' '+ dateA+ ', ' +yearA + ' to ' +dayB+', '+ monthB + ' '+ dateB+ ', ' +yearB}
-    />
-   </div>)
+      return (
+        <div className="statsPageContainer">
+          <div className="statsHeader">
+            <h1>User Statistics</h1>
+            <h2>( {this.state.fullName} )</h2>
+          </div>
+
+          <div className="chartsContainer">
+            <LineChart
+            className='StatsScoreGraph'
+            data={this.state.data}
+            margins={margins}
+            chartSeries={this.state.chartSeries1}
+            width={1100}
+            height={500}
+            title={'Score History'}
+            x={x}
+            xScale={xScale}
+            yAxisClassName= {'lineY'}
+            xAxisClassName= {'lineX'}
+            yLabel={'Scores'}
+            //x axis includes first and last day of play (for time range)
+            xLabel={'Gameplay from '+dayA+', '+ monthA + ' '+ dateA+ ', ' +yearA + ' to ' +dayB+', '+ monthB + ' '+ dateB+ ', ' +yearB} />
+          </div> {/* chartsContainer */}
+
+          <div className="statsDetailsContainer">
+            <table>
+              <tbody>
+                <tr>
+                  <td>Games Played:
+                  </td>
+                  <td className="statsTableData">{this.state.data.length}</td>
+                </tr>
+                <tr>
+                  <td>Highest N-Level:
+                  </td>
+                  <td className="statsTableData">{this.state.maxN}</td>
+                </tr>
+                <tr>
+                  <td>Highest Score:
+                  </td>
+                  <td className="statsTableData">{this.state.highScore}</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <Link className="leaderLink" to="/leaderboard">
+              <span className="lbChart">
+                <span className="fa fa-signal fa-5x"></span>
+                <h2>leaderboard</h2>
+              </span>
+            </Link>
+          </div>
+        </div>)
     }
    
   }
 });
 
 module.exports = MyComponent
+
+          // <AreaChart
+          // width={1100}
+          // height={500}
+          // title='TITLE'
+          // data= {this.state.data}
+          // className='StatsReactionGraph'
+          // margins={margins}
+          // chartSeries= {this.state.chartSeries2}
+          // yAxisClassName= {'areaY'}
+          // xAxisClassName= {'areaX'}
+          // x= {x}
+          // xScale={xScale}
+          // yLabel={'Reaction Times (ms)'}
+          // //x axis includes first and last day of play (for time range)
+          // xLabel={'Gameplay from '+dayA+', '+ monthA + ' '+ dateA+ ', ' +yearA + ' to ' +dayB+', '+ monthB + ' '+ dateB+ ', ' +yearB} /> 
