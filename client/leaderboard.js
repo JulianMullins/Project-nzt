9085bd9d9d4dbcc7ad1ecdb1e5f7a78933139cdb
Adam He <shuaihe2018@u.northwestern.edu>
2016-08-17 18:07:51 -0400

    new leaderboards

--- client/leaderboard.js
+++ client/leaderboard.js
@@ -6,7 +6,14 @@ import {Link} from 'react-router';
 
 var Leaderboard = React.createClass({
   getInitialState: function() {
-    return {allScores: [], myScores: [], global: true, hasScores: false}
+    return {
+      allScores: [],
+      myScores: [],
+      scoreBoard: [
+        1, 0, 0
+      ],
+      hasScores: false
+    }
   },
   componentDidMount: function() {
     this.getAllScores();
@@ -33,13 +40,26 @@ var Leaderboard = React.createClass({
   },
   render: function() {
 
-    var loggedIn = !this.state.hasScores && !this.state.global
+    var loggedIn = !this.state.hasScores && !this.state.scoreBoard[0]
       ? (
         <div className="gameOverPrompt">
-          <p><Link to="/gameOver/login">Login</Link> or <Link to="/gameOver/register">Sign Up</Link> to save your progress, view statistics and compete with friends!</p>
+          <p>
+            <Link to="/gameOver/login">Login</Link>
+            or
+            <Link to="/gameOver/register">Sign Up</Link>
+            to save your progress, view statistics and compete with friends!</p>
         </div>
       )
       : <div></div>;
+
+    var scores;
+    if (this.state.scoreBoard[0]) {
+      scores = this.state.allScores;
+    } else if (this.state.scoreBoard[1]) {
+      scores = this.state.myScores;
+    } else {
+      scores = this.state.myScores;
+    }
 
     return (
       <div className="leaderboardPage">
@@ -48,24 +68,37 @@ var Leaderboard = React.createClass({
           <section>
             <div className="leaderboardButtons">
               <a onClick={function() {
-                this.setState({global: true})
-              }.bind(this)} style={this.state.global
+                this.setState({
+                  scoreBoard: [1, 0, 0]
+                });
+              }.bind(this)} style={this.state.scoreBoard[0]
                 ? pressedStyle
                 : {}}>Global</a>
               <a onClick={function() {
-                this.setState({global: false})
-              }.bind(this)} style={!this.state.global
+                this.setState({
+                  scoreBoard: [0, 1, 0]
+                })
+              }.bind(this)} style={this.state.scoreBoard[1]
+                ? pressedStyle
+                : {}}>Friends</a>
+              <a onClick={function() {
+                this.setState({
+                  scoreBoard: [0, 0, 1]
+                })
+              }.bind(this)} style={this.state.scoreBoard[2]
                 ? pressedStyle
                 : {}}>Personal</a>
             </div>
-            <Reactable columns={this.state.global
-              ? columns
-              : columns.slice(1)} data={this.state.global
-              ? this.state.allScores
-              : this.state.myScores} currentPage={0} itemsPerPage={10} pageButtonLimit={5} sortable={true} defaultSort={{
+            <Reactable columns={!this.state.scoreBoard[2]
+              ? columns.slice(0, 5)
+              : columns.slice(0, 1).concat(columns.slice(2))} data={scores} currentPage={0} itemsPerPage={10} pageButtonLimit={5} sortable={true} defaultSort={{
               column: 'rank',
               direction: 'asc'
-            }} filterable={['username']} filtererPlaceholder='Search by username'/>
+            }} filterable={this.state.scoreBoard[2]
+              ? []
+              : ['username']} filterPlaceholder={this.state.scoreBoard[2]
+              ? ''
+              : 'Search by username'}/>
           </section>
           {loggedIn}
         </div>
@@ -96,6 +129,9 @@ var columns = [
   }, {
     key: 'level',
     label: 'Level'
+  }, {
+    key: 'date',
+    label: 'date'
   }
 ]
 
